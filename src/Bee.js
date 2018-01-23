8ba804df0fab81bf1191a200ee738a371ea6d9d2
armandocano91 <armandocano.ac@gmail.com>
2018-01-23 13:41:21 -0600

    finished Bee, Grub, Honeymaker and Forager Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 class Bee {
-  // TODO..
+  constructor (){
+  this.age = 5
+  this.color = "yellow"
+  this.food = "jelly"
+  this.job = "Keep on growing"
+  }
+  eat(){
+    return 'Mmmmmmmmm jelly'
+  }
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,14 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor (food, color, age, job, honeyPot){
+  super(food, color)
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
+};
+  makeHoney(){
+    this.honeyPot++;
+  };
+  giveHoney(){
+    this.honeyPot--;
+  };
 };
